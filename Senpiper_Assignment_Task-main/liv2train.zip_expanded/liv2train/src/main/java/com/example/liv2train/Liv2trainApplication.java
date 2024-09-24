0bc68a8bbb4e0aca8833c3b87561d4d2d1c28c2d
smrutimahajan14 <165942793+smrutimahajan14@users.noreply.github.com>
2024-09-24 12:18:05 +0530

    Add files via upload

---
+++ Senpiper_Assignment_Task-main/liv2train.zip_expanded/liv2train/src/main/java/com/example/liv2train/Liv2trainApplication.java
@@ -0,0 +1,13 @@
+package com.example.liv2train;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Liv2trainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Liv2trainApplication.class, args);
+	}
+
+}
